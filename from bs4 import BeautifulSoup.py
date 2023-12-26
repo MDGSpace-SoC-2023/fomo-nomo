0b187bf18f793b799457e88c8b22e1d796014e37
CheshtaB <chestabhandari0405@gmail.com>
2023-12-27 00:16:06 +0530

    test code

--- from bs4 import BeautifulSoup.py
+++ from bs4 import BeautifulSoup.py
@@ -5,24 +5,25 @@ import json
 from flask import Flask, request, jsonify, send_from_directory
 import openai
 from dotenv import load_dotenv
-import os
-load_dotenv()
 
-api_key=os.environ.get("OPENAI_API_KEY")
+load_dotenv()
+import os
+
+api_key = os.environ.get("OPENAI_API_KEY")
 app = Flask(__name__)
-def summary(text):
-    output=openai.Completion.create(
-        model="text-davinci-003",
-        prompt = f"generate summary of\n{text}\n in 150 words"
+
+def generate_summary(data):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"Summarize the following information in 150 words:\n{data}",
     )
-    return output.choices[0].text.strip()
+    return response.choices[0].text.strip()
 
 @app.route('/api/search', methods=['GET'])
 def search_api():
     query = request.args.get('query')
-    search_results = f'https://www.google.com/search?q={query}'
-    soup = BeautifulSoup(requests.get(search_results).text, 'html.parser')
     all_data = []
+
     for j in search(query, tld="co.in", num=1, stop=1, pause=2):
         data = requests.get(j)
         soup1 = BeautifulSoup(data.text, 'html.parser')
@@ -31,9 +32,11 @@ def search_api():
         if 'twitter.com' in j:
             print(f"Ignoring Twitter URL: {j}")
             continue
+
         if title:
             title = title.text.strip()
             paras = soup1.find_all('p')
+
             if paras:
                 paras_comb = '\n'.join(para.text.strip() for para in paras)
                 link_data = {'title': title, 'url': j, 'content': paras_comb}
@@ -42,9 +45,10 @@ def search_api():
                 print("No paragraphs found.")
         else:
             print(f"No h1 tag found on {j}")
-    final_data = json.dumps(all_data, indent=2)
-    summarized_data= summary(final_data)
-    return jsonify(summarized_data)
+
+    json_data = json.dumps(all_data, indent=2)
+    summary_text = generate_summary(json_data)
+    return jsonify({'summary': summary_text})
 
 @app.route('/favicon.ico')
 def favicon():
